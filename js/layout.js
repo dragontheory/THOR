c950f7a72cb9512905278e45c7cd45489dc0d215
[)4@6()|\| <80736+dragontheory@users.noreply.github.com>
2019-12-02 22:39:27 -0500

    Changed double quotes to single quotes. Removed duplicate fullscreen scripts.

--- js/layout.js
+++ js/layout.js
@@ -4,12 +4,12 @@
 //});
 // handle global search box expand/collapse
 jQuery(document).on('click', '.global-search-form', function(e) {
-    $(this).addClass("open");
+    $(this).addClass('open');
     $(this).find('.form-control').focus();
 
-    $(".global-search-form .form-control").on("blur", function(e) {
-        $(this).closest('.global-search-form').removeClass("open");
-        $(this).unbind("blur");
+    $('.global-search-form .form-control').on('blur', function(e) {
+        $(this).closest('.global-search-form').removeClass('open');
+        $(this).unbind('blur');
     });
 });
 // handle mega menu click
@@ -22,26 +22,34 @@ $(function () {
 });
 
 // handle RESULTS fullscreen
-$(".toggle-results-fullscreen").click(function(){
-    $("body").toggleClass("fullscreen");
+$('.toggle-results-fullscreen').click(function(){
+    $('.results').toggleClass('fullscreen');
 });
-$(".toggle-profile-fullscreen").click(function(){
-    $("body").toggleClass("fullscreen");
+$('.toggle-profile-fullscreen').click(function(){
+    $('.profile').toggleClass('fullscreen');
 });
-$(".toggle-details-fullscreen").click(function(){
-    $("body").toggleClass("fullscreen");
+$('.toggle-details-fullscreen').click(function(){
+    $('.details').toggleClass('fullscreen');
 });
+
 // handle toggle RESULTS panel width 100%-0%
-$(".toggle-results").click(function(){
-    $("body").toggleClass("show-results");
-    $("body").removeClass("hide-home hide-filter show-profile show-details");
+$('.toggle-results').click(function(){
+    $('body').toggleClass('show-results');
+    $('body').removeClass('hide-home hide-filter show-profile show-details');
 });
+
+// handle Loading PROFILE content
+$('body').removeClass('show-profile');
+if ($('.profile').hasClass('loaded')){
+    $('body').toggleClass('show-profile');
+}
+
 
 // handle flex box splitter
 $(document).ready(function () {
-    $(".results").resizable({
+    $('.results').resizable({
         // optional selector for handle that starts dragging
-        handleSelector: ".splitter",
+        handleSelector: '.splitter',
         // resize the width
         resizeWidth: true,
         // resize the height
@@ -63,75 +71,67 @@ $(document).ready(function () {
 });
 
 // handle toggle PROFILE panel width 100%-0%
-$(".toggle-profile").click(function(){
-    $("body").toggleClass("show-profile"); /* open/close profile panel */
-    $("body").removeClass("show-details");
-    $(".results table tbody tr").toggleClass("selected"); /* highlight selected row */
-    $(".profile").removeAttr("style"); /* remove inline style width put there dynamically by resizer */
-    $(".results").removeAttr("style"); /* remove inline style width put there dynamically by resizer */
+$('.toggle-profile').click(function(){
+    $('body').toggleClass('show-profile'); /* open/close profile panel */
+    $('body').removeClass('show-details');
+    $('.results table tbody tr').toggleClass('selected'); /* highlight selected row */
+    $('.profile').removeAttr('style'); /* remove inline style width put there dynamically by resizer */
+    $('.results').removeAttr('style'); /* remove inline style width put there dynamically by resizer */
 });
 
 // handle open PROFILE panel button(s)
-$(".open-profile").click(function(){
-    $("body").addClass("show-profile");
+$('.open-profile').click(function(){
+    $('body').addClass('show-profile');
 });
 
 // handle toggle DETAILS panel margin-right 0-300px
-$(".toggle-details").click(function(){
-    $("body").toggleClass("show-details");
+$('.toggle-details').click(function(){
+    $('body').toggleClass('show-details');
 });
 
 // handle toggle light/dark themes
-$(".toggle-lights").click(function(){
+$('.toggle-lights').click(function(){
     $('#lightsOut').prop('disabled', function(i, v) { return !v; }); /* hash is required to work vs class */
 });
 
 // handle toggle HOME panel min-width 250px-0
-$(".toggle-hide-home").click(function(){
-    $("body").toggleClass("hide-home");
+$('.toggle-hide-home').click(function(){
+    $('body').toggleClass('hide-home');
 });
 
 // handle toggle FILTER panel min-width 250px-0
-$(".toggle-hide-filter").click(function(){
-    $("body").toggleClass("hide-filter");
+$('.toggle-hide-filter').click(function(){
+    $('body').toggleClass('hide-filter');
 });
 
 // handle close PROFILE panel
-$(".close-profile").click(function(){
-    $("body").removeClass("show-profile");
-    $("body").removeClass("show-details");
-    $(".results table tbody tr").removeClass("selected"); /* highlight selected row */
-    $(".profile").removeAttr("style"); /* remove inline style width put there dynamically by resizer */
-    $(".results").removeAttr("style"); /* remove inline style width put there dynamically by resizer */
+$('.close-profile').click(function(){
+    $('body').removeClass('show-profile');
+    $('body').removeClass('show-details');
+    $('.results table tbody tr').removeClass('selected'); /* highlight selected row */
+    $('.profile').removeAttr('style'); /* remove inline style width put there dynamically by resizer */
+    $('.results').removeAttr('style'); /* remove inline style width put there dynamically by resizer */
 });
 // handle close DETAILS panel
-$(".close-details").click(function(){
-    $("body").removeClass("show-details");
-    $(".profile").removeAttr("style"); /* remove inline style width put there dynamically by resizer */
-    $(".results").removeAttr("style"); /* remove inline style width put there dynamically by resizer */
-});
-
-// handle toggle RESULTS fullscreen
-$(".toggle-results-fullscreen").click(function(){
-    $(".results").toggleClass("fullscreen");
-});
-
-// handle toggle PROFILE fullscreen
-$(".toggle-profile-fullscreen").click(function(){
-    $(".profile").toggleClass("fullscreen");
-});
-
-// handle toggle DETAILS fullscreen
-$(".toggle-details-fullscreen").click(function(){
-    $(".details").toggleClass("fullscreen");
+$('.close-details').click(function(){
+    $('body').removeClass('show-details');
+    $('.profile').removeAttr('style'); /* remove inline style width put there dynamically by resizer */
+    $('.results').removeAttr('style'); /* remove inline style width put there dynamically by resizer */
 });
 
 // dismiss modal on ESC
-$(document).keyup(function(event){
+/*$(document).keyup(function(event){
     if(event.which=='27'){
-        $(".results").removeClass("fullscreen");
-        $(".profile").removeClass("fullscreen");
-        $(".details").removeClass("fullscreen");
+        $('.results').removeClass('fullscreen');
+        $('.profile').removeClass('fullscreen');
+        $('.details').removeClass('fullscreen');
+    }
+});*/
+$(document).on('keydown', function(event) {
+    if (event.key == 'Escape') {
+        $('.results').removeClass('fullscreen');
+        $('.profile').removeClass('fullscreen');
+        $('.details').removeClass('fullscreen');
     }
 });
 
